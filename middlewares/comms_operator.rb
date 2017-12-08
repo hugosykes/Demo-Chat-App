ddc5c382729817b3e96f759d20f1ac0e16bae79d
hugosykes <hugo@denmeadfarm.co.uk>
2017-12-08 17:27:53 +0000

    Adds methods to add username to the directory and return their WSID's if they exist

--- middlewares/comms_operator.rb
+++ middlewares/comms_operator.rb
@@ -1,5 +1,15 @@
 class CommsOperator
 
-#
+  def initialize(dir = [])
+    @username_WSID_directory = dir
+  end
 
+  def add_username(name)
+    @username_WSID_directory.push({name: name, WSID: nil})
+  end
+
+  def find_WSID(name)
+    @username_WSID_directory.each { |user| return user[:WSID] if user[:name] == name }
+    false
+  end
 end
--- spec/comms_operator_spec.rb
+++ spec/comms_operator_spec.rb
@@ -2,18 +2,23 @@ require(File.expand_path('../middlewares/comms_operator.rb', File.dirname(__FILE
 
 describe CommsOperator do
 
-@username_WSID_directory = [{name:'Hugo', WSID: nil}]
+  username_WSID_directory = [
+    {name: 'Hugo', WSID: nil},
+    {name: 'Ollie', WSID: 10101010101010}
+  ]
 
+  let(:subject) { CommsOperator.new(username_WSID_directory) }
 
   it 'adds new usernames to the username_WSID_directory' do
-
+    subject.add_username('Lucy')
+    expect(username_WSID_directory.last[:name]).to eq "Lucy"
   end
 
   it 'searches username_WSID_directory for a recipient\'s name and returns their WSID' do
+    expect(subject.find_WSID('Ollie')).to eq 10101010101010
   end
 
-  it 'throws error if recipients name doesn\'t exist in the username_WSID_directory via user\'s WSID' do
+  it 'returns false if recipients name doesn\'t exist in the username_WSID_directory' do
+    expect(subject.find_WSID('Chris')).to eq false
   end
-
-
 end
