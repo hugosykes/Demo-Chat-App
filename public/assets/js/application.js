1c0b63c28b0922ab7a376d5c6b290c70a252b974
oliverpople <oliverpople@gmail.com>
2017-12-11 07:41:32 +0000

    Added error message to input textSanitiser method.

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -1,45 +1,51 @@
 $(document).ready(function() {
 
-    var scheme = "ws://";
-    var uri = scheme + window.document.location.host + '/';
-    var ws = new WebSocket(uri);
-    var messages = [];
+  var scheme = "ws://";
+  var uri = scheme + window.document.location.host + '/';
+  var ws = new WebSocket(uri);
+  var messages = [];
 
-    ws.onmessage = function(message) {
-      var data = JSON.parse(message.data);
-      $("#chat-text").append("<br><scan>" + data.handle + " says: " + data.text + "");
-    };
+  ws.onmessage = function(message) {
+    var data = JSON.parse(message.data);
+    $("#chat-text").append("<br><scan>" + data.handle + " says: " + data.text + "");
+  };
 
-    function send() {
-      var handle = $('#input-handle').val();
-      var text = $("#input-text").val();
-      ws.send(JSON.stringify({
-        handle: handle,
-        text: text
-      }));
-      $("#input-text").val('');
+  function send() {
+    var handle = $('#input-handle').val();
+    var text = $("#input-text").val();
+    ws.send(JSON.stringify({
+      handle: handle,
+      text: text
+    }));
+    $("#input-text").val('');
+  }
+
+  $('#input-text').keypress(function(event) {
+    if (textSanitiser('#input-text') && event.keyCode == '13') {
+      send();
+    } else {
+      textSanitiseError();
     }
-
-    $('#input-text').keypress(function(event) {
-      if (textSanitiser('#input-text') && event.keyCode == '13') {
-        send();
-      } else {
-        console.log("Text input not valid. Try removing symbols.");
-      }
-    });
-
-    $('#submit').click(function() {
-      if (textSanitiser('#input-text')) {
-        send();
-      }
-    });
-
-    function textSanitiser(text_input) {
-      if (text_input.includes('<' || '>')) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-
   });
+
+  $('#submit').click(function() {
+    if (textSanitiser('#input-text')) {
+      send();
+    } else {
+      textSanitiseError();
+    }
+  });
+
+  function textSanitiser(text_input) {
+    if (text_input.includes('<' || '>')) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function textSanitiseError() {
+    $("#chat-text").append("<br><scan> Text input not valid. Remove symbols and try again.");
+  }
+
+});
