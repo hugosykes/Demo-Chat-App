b9c413fccd46ecdcac3ddd730d1987c6c44b8da0
hugosykes <hugo@denmeadfarm.co.uk>
2017-12-12 11:35:12 +0000

    Printing to heroku logs drops connections, so trying another approach

--- middlewares/comms_operator.rb
+++ middlewares/comms_operator.rb
@@ -5,13 +5,13 @@ class CommsOperator
   end
 
   def add_username(name, id)
-    @username_WSID_directory.each {|user| puts "BEFORE METHOD user: " + user[:name] + ", and object_id: " + user[:WSID] + "" }    
+    @username_WSID_directory.each {|user| puts "BEFORE METHOD user: ", user[:name], ", and object_id: ", user[:WSID]}    
     @username_WSID_directory.each { |user| return if !user[:name] }
     @username_WSID_directory.push({
       name: name, 
       WSID: id
     })
-    @username_WSID_directory.each {|user| puts "AFTER METHOD user: " + user[:name] + ", and object_id: " + user[:WSID] + "" }
+    @username_WSID_directory.each {|user| puts "AFTER METHOD user: ", user[:name], ", and object_id: ", user[:WSID]}
   end
 
   def find_WSID(name)
@@ -21,7 +21,7 @@ class CommsOperator
 
   def check_genuine_message(json, wsid)
     parsed_json = JSON.parse(json)
-    puts "Check_genuine: parsed_json['text']: " + parsed_json["text"]
+    puts "Check_genuine: parsed_json['text']: ", parsed_json["text"]
     if parsed_json["text"] == ""
       add_username(parsed_json["senderName"], wsid)
       return false
@@ -34,7 +34,7 @@ class CommsOperator
     parsed_message = JSON.parse(message)
     ws_ids = [find_WSID(parsed_message["receiverName"]), find_WSID(parsed_message["senderName"])]
     clients.each { |client| client.send(message) if ws_ids.include?(client.object_id) }
-    clients.each { |client| puts ("client.object_id upon sending: " + client.object_id) if ws_ids.include?(client.object_id) }
+    clients.each { |client| puts ("client.object_id upon sending: ", client.object_id) if ws_ids.include?(client.object_id) }
   end
 
   def disconnects(name)
